30f4bfbbfac0fbd26c82b19ef9b73df25a8b3aa2
CianHardiman <83504759+CianHardiman@users.noreply.github.com>
2021-05-03 10:15:35 +0100

    Refactored and Renamed

--- src/main/java/GRPCServer.java
+++ src/main/java/GRPCServer.java
@@ -2,13 +2,13 @@ import java.io.IOException;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import user.userGUI;
+import user.UserStaffing;
 
 public class GRPCServer
 {
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
-		Server server = ServerBuilder.forPort(50052).addService(new userGUI()).build();
+		Server server = ServerBuilder.forPort(50052).addService(new UserStaffing()).build();
 		
 		server.start();
 		
--- src/main/java/user/UserStaffing.java
+++ src/main/java/user/UserStaffing.java
@@ -4,8 +4,12 @@ import com.project.staffingGrpc.*;
 
 import io.grpc.stub.StreamObserver;
 
-public class userGUI extends staffingImplBase
+public class UserStaffing extends staffingImplBase
 {
+	int levelNumber = 0;
+	int timeNumber = 0;
+	int nursesNeeded = 0;
+	int doctorsNeeded = 0;
 
 	public void staffRequired(TimeLevel request, StreamObserver<APIResponse> responseObserver) 
 	{
@@ -13,13 +17,9 @@ public class userGUI extends staffingImplBase
 		System.out.println("Time 1 is 12AM-8AM, 2 is 8AM-4PM (Working Hours), 3 is 4PM-12AM");
 		System.out.println("Level 1 is quieter than usual, 2 is medium and 3 is busy");
 		
-		int levelNumber = 0;
-		int timeNumber = 0;
-		int nursesNeeded;
-		int doctorsNeeded = 0;
-	
+
+		APIResponse.Builder response = APIResponse.newBuilder();
 		
-		//set timeNumber
 		try 
 		{
 			timeNumber = Integer.parseInt(request.getTime());
@@ -40,9 +40,24 @@ public class userGUI extends staffingImplBase
 			System.out.println("Please enter level as an number");
 		}
 		
-		APIResponse.Builder response = APIResponse.newBuilder();
+		staffRequiredFunction();
+
+		if ((levelNumber<=3 && levelNumber>=1)&&(timeNumber<=3 && timeNumber>=1))
+		{
+			response.setResponseCode(0).setResponseText("Nurses Needed: " + nursesNeeded + ". Doctors Needed: " + doctorsNeeded + ".");
+		}
+		else
+		{
+			response.setResponseCode(1).setResponseText("Please ensure that time and level are both 1, 2 or 3");
+		}
 		
+		responseObserver.onNext(response.build());
+		responseObserver.onCompleted();
 		
+	}
+	public void staffRequiredFunction()
+	{
+			
 		if(levelNumber==1)
 		{
 			if (timeNumber == 1 || timeNumber == 3)
@@ -98,19 +113,5 @@ public class userGUI extends staffingImplBase
 			}
 		
 		nursesNeeded = doctorsNeeded*2;
-		
-		if ((levelNumber<=3 && levelNumber>=1)&&(timeNumber<=3 && timeNumber>=1))
-		{
-			response.setResponseCode(0).setResponseText("Nurses Needed: " + nursesNeeded + ". Doctors Needed: " + doctorsNeeded + ".");
-		}
-		else
-		{
-			response.setResponseCode(1).setResponseText("Please ensure that time and level are both 1, 2 or 3");
-		}
-		
-		responseObserver.onNext(response.build());
-		responseObserver.onCompleted();
-		
 	}
-	
 }
